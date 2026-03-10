7184034e1aa0979045dc9ef0e7dda2b5793bd474
bohuslavstan <bohuslav.stan@gmail.com>
2026-03-10 03:39:05 +0200

    solved

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,11 +7,13 @@ function calculateRentalCost(days) {
   // write code here
   const dailyRent = 40;
   let discount = 0;
-  if (days >= 7) {
-    discount = 50;
-  }
+
   if (days >= 3) {
     discount = 20;
+  }
+
+  if (days >= 7) {
+    discount = 50;
   }
 
   return days * dailyRent - discount;
